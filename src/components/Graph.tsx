9dc8eff603ccd9a005f31181780ae3cdf87f40af
Samuel Forrest <samueljforrest@outlook.com>
2024-10-21 13:31:07 +0100

    clean up

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -6,27 +6,10 @@ import timeseries from "@/assets/data/timeseries.json"
 
 const Graph = () => {
   
-  const points: GraphPoint[] = [
-    {
-      date: new Date(2024, 1, 1),
-      value: 10,
-    },
-    {
-      date: new Date(2024, 1, 2),
-      value: 15,
-    },
-    {
-      date: new Date(2024, 1, 3),
-      value: 5,
-    }
-  ];
-
   const graphPoints: GraphPoint[] = timeseries.values.map((value) => 
     ({date: new Date(value.datetime),
       value: Number.parseFloat(value.close),
   }));
-
-  console.log(JSON.stringify(graphPoints, null, 2));
 
   return (
     <View>
