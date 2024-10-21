39018e0b11aaac545796bca3adb08e899b4da0ab
Samuel Forrest <samueljforrest@outlook.com>
2024-10-21 12:51:08 +0100

    dynamic path parameter

---
+++ src/app/[symbol].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "@/src/components/Themed";
+import React from 'react'
+
+const StockDetails = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
+
+export default StockDetails;
--- src/components/StockListItem.tsx
+++ src/components/StockListItem.tsx
@@ -1,8 +1,9 @@
 import {Text, View} from "./Themed";
 import {MonoText} from "./StyledText";
-import {StyleSheet} from "react-native";
+import {StyleSheet, Pressable} from "react-native";
 import Colors from "../constants/Colors";
 import { AntDesign } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 type Stock = {
   name: string;
@@ -19,22 +20,24 @@ export default function StockListItem({stock}: StockListItem) {
 
   const change = Number.parseFloat(stock.percent_change);
 
-  return ( //We may only have one item in the return tag - the view
-    <View style={styles.container}>
-      
-      {/*Left Container*/}
-      <View style={{ flex:1 }}>
-        <Text style={styles.symbol}>{stock.symbol} <AntDesign name="staro" size={18} color="gray" /></Text>
-        <Text style={{color: "gray"}}>{stock.name}</Text>
-      </View>
-      
-      {/*Right Container*/}
-      <View style={{ alignItems: "flex-end" }}>
-        <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
-        <MonoText style={{ color: change > 0? "green" : "red" }}>{change > 0 ? "+" : ""}{change.toFixed(1)}%</MonoText>
-      </View>
+  return ( //We may only have one item in the return tag - the view / pressable view
+    <Link href={"/stock"} asChild>
+      <Pressable style={styles.container}>
+        
+        {/*Left Container*/}
+        <View style={{ flex:1, gap: 5 }}>
+          <Text style={styles.symbol}>{stock.symbol} <AntDesign name="staro" size={18} color="gray" /></Text>
+          <Text style={{color: "gray"}}>{stock.name}</Text>
+        </View>
+        
+        {/*Right Container*/}
+        <View style={{ alignItems: "flex-end" }}>
+          <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
+          <MonoText style={{ color: change > 0? "green" : "red" }}>{change > 0 ? "+" : ""}{change.toFixed(1)}%</MonoText>
+        </View>
 
-    </View>
+      </Pressable>
+    </Link>
   )
 }
 
